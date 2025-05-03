13cdafaedf2a911961de3867affa794bbf5c3644
Sameh Muhammed <samehznaty@gmail.com>
2025-05-03 15:37:40 +0300

    feat(task.service): add CRUD operations and AI-related features for tasks

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -4,11 +4,10 @@ import {
   BehaviorSubject,
   catchError,
   Observable,
-  of,
   tap,
   throwError,
 } from 'rxjs';
-import { Task } from '../models/task.model';
+import { Task, Comment, Checklist, ChecklistItem } from '../models/task.model';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -43,19 +42,117 @@ export class TaskService {
     );
   }
 
-  updateTask(task: Task): Observable<Task> {
-    return this.http.put<Task>(`${this.apiUrl}/${task.id}`, task).pipe(
-      tap((newTask) => {
+  updateTask(taskId: string, task: Partial<Task>): Observable<Task> {
+    return this.http.patch<Task>(`${this.apiUrl}/${taskId}`, task).pipe(
+      tap((updatedTask) => {
         const currentTasks = this.taskSubject.getValue();
-        const index = currentTasks.findIndex((x) => x.id === newTask.id);
+        const index = currentTasks.findIndex((item) => item.id === taskId);
         if (index !== -1) {
-          const uptaedTasks = [...currentTasks];
-          uptaedTasks[index] = newTask;
-          this.taskSubject.next(uptaedTasks);
+          const newTasks = [...currentTasks];
+          newTasks[index] = updatedTask;
+          this.taskSubject.next(newTasks);
         }
       }),
-      catchError((err) => this.handleError(err))
+      catchError(this.handleError)
     );
+  }
+  moveTask(taskId: string, targetStageId: string): Observable<Task> {
+    return this.http
+      .patch<Task>(`${this.apiUrl}/${taskId}/move`, {
+        stageId: targetStageId,
+      })
+      .pipe(
+        tap((updatedTask) => {
+          const currentTasks = this.taskSubject.getValue();
+          const index = currentTasks.findIndex((x) => x.id === taskId);
+          if (index !== -1) {
+            const updatedTasks = [...currentTasks];
+            updatedTasks[index] = updatedTask;
+            this.taskSubject.next(updatedTasks);
+          }
+        }),
+        catchError(this.handleError)
+      );
+  }
+  deleteTask(taskId: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${taskId}`).pipe(
+      tap(() => {
+        const currentTasks = this.taskSubject.getValue();
+        this.taskSubject.next(currentTasks.filter((x) => x.id !== taskId));
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  addComment(taskId: string, comment: Partial<Comment>): Observable<Comment> {
+    return this.http
+      .post<Comment>(`${this.apiUrl}/${taskId}/comments`, comment)
+      .pipe(
+        tap((comment) => {
+          const currentTasks = this.taskSubject.getValue();
+          const index = currentTasks.findIndex((t) => t.id === taskId);
+          if (index !== -1) {
+            const newTasks = [...currentTasks];
+            newTasks[index] = {
+              ...newTasks[index],
+              comments: [...newTasks[index].comments, comment],
+            };
+          }
+        }),
+        catchError(this.handleError)
+      );
+  }
+  updateCheckList(
+    taskId: string,
+    checkList: Partial<Checklist>
+  ): Observable<Checklist> {
+    return this.http
+      .put<Checklist>(
+        `${this.apiUrl}/${taskId}/checklists/${checkList.id}`,
+        checkList
+      )
+      .pipe(
+        tap((updatedChecklist) => {
+          const currentTasks = this.taskSubject.getValue();
+          const index = currentTasks.findIndex((i) => i.id === taskId);
+          if (index !== -1) {
+            const updatedTaskes = [...currentTasks];
+            const checklistIndex = updatedTaskes[index].checklists.findIndex(
+              (x) => x.id === checkList.id
+            );
+            if (checklistIndex !== -1) {
+              updatedTaskes[index].checklists[checklistIndex] =
+                updatedChecklist;
+              this.taskSubject.next(updatedTaskes);
+            }
+          }
+        }),
+        catchError(this.handleError)
+      );
+  }
+
+  getAiSuggestedTasks(projectType: string): Observable<Partial<Task[]>> {
+    return this.http
+      .get<Partial<Task[]>>(
+        `${this.apiUrl}/ai-suggessted?projectType=${projectType}`
+      )
+      .pipe(catchError(this.handleError));
+  }
+  getAiSuggesstedChecklist(
+    taskId: string
+  ): Observable<Partial<ChecklistItem[]>> {
+    return this.http
+      .get<Partial<ChecklistItem[]>>(`${this.apiUrl}/${taskId}/ai-checklist`)
+      .pipe(catchError(this.handleError));
+  }
+  predictDeadline(
+    taskId: string
+  ): Observable<{ predictDate: Date; cofidence: number }> {
+    return this.http
+      .get<{ predictDate: Date; cofidence: number }>(
+        `${this.apiUrl}/${taskId}/predict-deadline`
+      )
+      .pipe(catchError(this.handleError));
   }
   private handleError(error: any) {
     console.error(`err:`, error);
