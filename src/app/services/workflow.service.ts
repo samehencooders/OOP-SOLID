38149a200f8a7cbad7686fe031501a2515b9eafe
Sameh Muhammed <samehznaty@gmail.com>
2025-05-03 18:56:02 +0300

    feat(workflow): add deleteWorkflow, addStage, and updateStage methods
    deleteWorkflow, addStage, and updateStage. These methods enable the deletion of workflows and the addition/updating of stages within workflows, ensuring the local state is updated accordingly

--- src/app/services/workflow.service.ts
+++ src/app/services/workflow.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, catchError, Observable, tap, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { Workflow } from '../models/workflow.model';
+import { Workflow, WorkflowStage } from '../models/workflow.model';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -61,6 +61,91 @@ export class WorkflowService {
           }
           if (this.currentWorkflowSubject.getValue()?.id === workflowId) {
             this.currentWorkflowSubject.next(updatedWorkflow);
+          }
+        }),
+        catchError(this.handleError)
+      );
+  }
+  deleteWorkflow(workflowId: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${workflowId}`).pipe(
+      tap(() => {
+        const currentWorkflows = this.workflowsSubject.getValue();
+        this.workflowsSubject.next(
+          currentWorkflows.filter((x) => x.id !== workflowId)
+        );
+
+        if (this.currentWorkflowSubject.getValue()?.id === workflowId) {
+          this.currentWorkflowSubject.next(null);
+        }
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  addStage(
+    workflowId: string,
+    stage: Partial<WorkflowStage>
+  ): Observable<WorkflowStage> {
+    return this.http
+      .post<WorkflowStage>(`${this.apiUrl}/${workflowId}/stages`, stage)
+      .pipe(
+        tap((newStage) => {
+          const currentWorkflow = this.currentWorkflowSubject.getValue();
+          if (currentWorkflow && currentWorkflow.id === workflowId) {
+            const updatedWorkflow = {
+              ...currentWorkflow,
+              stages: [...currentWorkflow.stages, newStage],
+            };
+            this.currentWorkflowSubject.next(updatedWorkflow);
+            const currentWorkflows = this.workflowsSubject.getValue();
+            const index = currentWorkflows.findIndex(
+              (x) => x.id === workflowId
+            );
+            if (index !== -1) {
+              const updatedWorkflows = [...currentWorkflows];
+              updatedWorkflows[index] = updatedWorkflow;
+              this.workflowsSubject.next(updatedWorkflows);
+            }
+          }
+        }),
+        catchError(this.handleError)
+      );
+  }
+  updateStage(
+    workflowId: string,
+    stageId: string,
+    updates: Partial<WorkflowStage>
+  ): Observable<WorkflowStage> {
+    return this.http
+      .patch<WorkflowStage>(
+        `${this.apiUrl}/${workflowId}/stages/${stageId}`,
+        updates
+      )
+      .pipe(
+        tap((updatedStage) => {
+          const currentWorkFlow = this.currentWorkflowSubject.getValue();
+          if (currentWorkFlow && currentWorkFlow.id === workflowId) {
+            const stageIndex = currentWorkFlow.stages.findIndex(
+              (x) => x.id === stageId
+            );
+            if (stageIndex !== -1) {
+              const updatedStages = [...currentWorkFlow.stages];
+              updatedStages[stageIndex] = updatedStage;
+              const updatedWorkflow = {
+                ...currentWorkFlow,
+                stages: updatedStages,
+              };
+              this.currentWorkflowSubject.next(updatedWorkflow);
+              const currentWorkflows = this.workflowsSubject.getValue();
+              const workflowIndex = currentWorkflows.findIndex(
+                (x) => x.id === workflowId
+              );
+              if (workflowIndex !== -1) {
+                const updatedWorkflows = [...currentWorkflows];
+                updatedWorkflows[workflowIndex] = updatedWorkflow;
+                this.workflowsSubject.next(updatedWorkflows);
+              }
+            }
           }
         }),
         catchError(this.handleError)
